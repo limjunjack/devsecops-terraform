a92e8ad8b4e8234603c12395a378dfdc97af30b5
Lim Jun Jack <d1345110@ncsdevnet.com>
2023-07-13 14:38:26 +0800

    newnew1

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
-terrafrom{
+terrafrom {
 	required_providers{
 		docker ={
 			source = "kreuzwerker/docker"
@@ -9,7 +9,7 @@ terrafrom{
 			version = "2.26.0"
 		}
 		local = {
-			source = "hashicorp/loca"
+			source = "hashicorp/local"
 			version = "2.4.0"
 		}
 	}
